ce603f8b1264620e95b07dc318e2b8fa21304118
Amit Sharma <99009356+amitshharma@users.noreply.github.com>
2023-06-07 23:05:20 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white'>
+
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full'>
+
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4
+                border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>
+            Student who focuses on writing clean, elegant and efficient code. 
+            Always looking forward to grab challenging opportunities and working with 
+            sincerity utmost dedication to cross performance benchmarks and achieving 
+            targets in time. Seeking an opportunity to work and learn in the field of 
+            web-development which provides me the opportunity to improve my skills and knowledge.
+            </p>
+            <br/>
+        </div>
+    </div>
+  )
+}
+
+export default About
