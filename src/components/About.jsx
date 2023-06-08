b8de394f0f3edcb1093793ea01c8b11035e48811
Amit Sharma <99009356+amitshharma@users.noreply.github.com>
2023-06-08 21:45:24 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,15 +15,17 @@ const About = () => {
 
             <p className='text-xl mt-20'>
             I am a meticulous and detail-oriented developer who places a strong emphasis on writing clean, 
-    elegant, and efficient code. With a passion for web development, I constantly seek out challenging opportunities 
-    that push my boundaries and allow me to grow both personally and professionally.
-When approaching projects, I bring a high level of dedication and commitment to deliver exceptional results. 
-    I thrive on surpassing performance benchmarks and meeting targets within set timeframes, while maintaining a 
-    focus on quality and accuracy.
-I am eager to continue my journey in the field of web development, as it provides a constant opportunity for learning 
-    and improvement. I am driven by the desire to enhance my skills and knowledge, staying up-to-date with the latest 
-    industry trends and technologies. By embracing new challenges and learning experiences, I aim to continually evolve 
-    as a developer and contribute my best work to the web development community..
+            elegant, and efficient code. With a passion for web development, I constantly seek out challenging opportunities 
+            that push my boundaries and allow me to grow both personally and professionally.
+            <br/>
+            When approaching projects, I bring a high level of dedication and commitment to deliver exceptional results. 
+            I thrive on surpassing performance benchmarks and meeting targets within set timeframes, while maintaining a 
+            focus on quality and accuracy.
+            <br/>
+            I am eager to continue my journey in the field of web development, as it provides a constant opportunity for learning 
+            and improvement. I am driven by the desire to enhance my skills and knowledge, staying up-to-date with the latest 
+            industry trends and technologies. By embracing new challenges and learning experiences, I aim to continually evolve 
+            as a developer and contribute my best work to the web development community.
             </p>
             <br/>
         </div>
